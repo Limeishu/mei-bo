62cc627da67de51ef9a4cf0af988ce012d83e081
Aries Cs <aries0d0f@gmail.com>
2019-06-30 17:44:43 +0800

    [Method] Stream api

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,13 @@ import KoaJson from 'koa-json';
 import BodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
 
+import api from './api';
+
 const app = new Koa();
 
 app.use(KoaJson);
 app.use(BodyParser);
 app.use(cors());
+app.use(api.routes());
+
+app.listen(process.env.PORT || 3000);
