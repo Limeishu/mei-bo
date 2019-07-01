eea533f38bfc62b8acbd0313d4d18c93dfed3c59
Aries Cs <aries0d0f@gmail.com>
2019-07-01 13:20:12 +0800

    [Method] Add tg server message resolver

--- server/util/resolver.ts
+++ server/util/resolver.ts
@@ -3,3 +3,35 @@ export interface ITGServerResponse {
   result: boolean | any;
   description?: string;
 }
+
+export interface ITGServerMessageEntity {
+  offset: number;
+  length: number;
+  type: 'mention' | 'bot_command';
+}
+
+export interface ITGServerMessage {
+  update_id: number;
+  message:
+  {
+    message_id: number,
+    from:
+    {
+      id: number,
+      is_bot: boolean,
+      first_name: string,
+      username: string,
+      language_code: string
+    },
+    chat:
+    {
+      id: number,
+      first_name: string,
+      username: string,
+      type: string
+    },
+    date: number,
+    text: string,
+    entities: ITGServerMessageEntity[]
+  };
+}
